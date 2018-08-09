b4d1c99ae1832df408308df3e3f9bf55359266a5
Maksym Kravtsov <kravtsov.maxim@gmail.com>
2018-08-08 20:10:30 -0400

    Refactored code in order to separate ui from main.

--- Makefile
+++ Makefile
@@ -1,18 +1,18 @@
 CC := gcc
 CFLAGS := -O2
+LDFLAGS += -lncurses
 OBJECTS = $(patsubst %.c,%.o,$(wildcard *.c))
 PROG_NAME = cross_zeros
 
 .PHONY: all clear
 
 #.o:.c
-#	${CC} ${CFLAGS} -o $$ 
+#	${CC} ${CFLAGS} -o $$
 
 $(PROG_NAME):$(OBJECTS)
-	${CC} -o ${PROG_NAME} $^
+	$(CC) $(LDFLAGS) -o $(PROG_NAME) $^
 
 all: $(PROG_NAME)
 
 clean:
 	-rm $(OBJECTS) $(PROG_NAME)
-
--- main.c
+++ main.c
@@ -6,51 +6,13 @@
 #include "include/color.h"
 #include "include/field.h"
 #include "include/game.h"
+#include "include/ui.h"
+#include "include/ui_old.h"
 
 #define NOOB_STRING "noob"
 #define EASY_STRING "easy"
 #define NORMAL_STRING "normal"
 #define HARD_STRING "hard"
-
-static inline void print_cell( enum cell_status status, int cell_index )
-{
-	switch( status )
-	{
-		case CELL_CPU:
-			// printf( BRIGHT_YELLOW_BOLD "X" RESET_COLOR );
-			printf( BRIGHT_WHITE BG_BRIGHT_BLACK "X" RESET_COLOR );
-			break;
-
-		case CELL_USER:
-			// printf( BRIGHT_GREEN_BOLD "O" RESET_COLOR);
-			printf( BRIGHT_BLACK BG_BRIGHT_GREEN "O" RESET_COLOR);
-			break;
-
-		default:
-			printf( "%d", cell_index );
-			break;
-	}
-
-}
-
-void print_field( game_t *game )
-{
-	field_t *field = game_get_field( game );
-	// puts("\n" );
-
-	for( uint8_t i = MIN_CELL; i <= MAX_CELL; ++i )
-	{
-		if( ( i % 3 == 0 ) )
-		{
-			printf("\n" );
-		}
-
-		enum cell_status status = field_get_cell_status( field, i );
-		print_cell( status, i );
-	}
-
-	printf("\n" );
-}
 
 static void print_usage()
 {
@@ -66,58 +28,8 @@ static void print_usage()
 	exit( 0 );
 }
 
-static void print_winner( enum cell_status winner )
-{
-	switch( winner )
-	{
-		case CELL_CPU:
-			puts( BG_BRIGHT_RED "CPU WON!" RESET_COLOR );
-			break;
-
-		case CELL_USER:
-			puts( BRIGHT_RED "PLAYER WON!" RESET_COLOR );
-			break;
-
-		case CELL_INVALID:
-		default:
-			puts( BRIGHT_WHITE "DRAW!" RESET_COLOR );
-			break;
-	}
-}
-
 int main( int argc, char** argv )
 {
-
-	// puts( WHITE BG_BLACK "X" RESET_COLOR );
-	// puts( WHITE_BOLD BG_BLACK "X" RESET_COLOR );
-	// puts( WHITE BG_RED "X" RESET_COLOR );
-	// puts( WHITE_BOLD BG_RED "X" RESET_COLOR );
-	// puts( WHITE BG_GREEN "X" RESET_COLOR );
-	// puts( WHITE_BOLD BG_GREEN "X" RESET_COLOR );
-	// puts( WHITE BG_BLUE "X" RESET_COLOR );
-	// puts( WHITE_BOLD BG_BLUE "X" RESET_COLOR );
-	// puts( WHITE BG_YELLOW "X" RESET_COLOR );
-	// puts( WHITE_BOLD BG_YELLOW "X" RESET_COLOR );
-	// puts( WHITE BG_MAGENTA "X" RESET_COLOR );
-	// puts( WHITE_BOLD BG_MAGENTA "X" RESET_COLOR );
-	// puts( WHITE BG_CYAN "X" RESET_COLOR );
-	// puts( WHITE_BOLD BG_CYAN "X" RESET_COLOR );
-	//
-	// puts( BRIGHT_WHITE BG_BRIGHT_BLACK "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE_BOLD BG_BRIGHT_BLACK "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE BG_BRIGHT_RED "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE_BOLD BG_BRIGHT_RED "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE BG_BRIGHT_GREEN "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE_BOLD BG_BRIGHT_GREEN "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE BG_BRIGHT_BLUE "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE_BOLD BG_BRIGHT_BLUE "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE BG_BRIGHT_YELLOW "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE_BOLD BG_BRIGHT_YELLOW "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE BG_BRIGHT_MAGENTA "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE_BOLD BG_BRIGHT_MAGENTA "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE BG_BRIGHT_CYAN "X" RESET_COLOR );
-	// puts( BRIGHT_WHITE_BOLD BG_BRIGHT_CYAN "X" RESET_COLOR );
-
 	enum game_difficulty difficulty = GAME_DIFFICULTY_HARD;
 
 	int c;
@@ -154,78 +66,40 @@ int main( int argc, char** argv )
 		}
 	}
 
+	ui_t *ui = &ui_old;
 	game_t *game = game_create();
 	game_init( game, difficulty );
 
-	print_field( game );
+	ui->vtable->print_field( game );
 	char answer;
 	enum cell_status winner = CELL_INVALID;
 
 	do {
-		print_cell( CELL_USER, 0 );
-		puts( " - player" );
-		print_cell( CELL_CPU, 0 );
-		puts( " - cpu" );
-		print_cell( CELL_EMPTY, 0 );
-		puts( " - empty cell" );
-
+		ui->vtable->print_legend();
 		while( !game_is_over( game, &winner ) )
 		{
 			// puts( "Game is not over\n" );
 
 			if( game_is_user_move( game ) && !game_is_over( game, &winner ) )
 			{
-				print_field( game );
+				ui->vtable->print_field( game );
 
-				int index = 0;
-				puts( "Input cell index: " );
-				int s = 0;
-
-				do
-				{
-					s = scanf( "%d", &index );
-
-					if( EOF == s )
-					{
-						return -1;
-					}
-					else if( 0 == s )
-					{
-						while (fgetc(stdin) != '\n');
-					}
-				}while( 1 != s );
+				int index = ui->vtable->read_cell_index();
 
 
 				if( !game_user_move( game, index ) )
 				{
-					printf("Invalid cell index " BRIGHT_WHITE "%d" RESET_COLOR "\n", index );
+					ui->vtable->print_invalid_index( index );
 				}
 				// print_field( game );
 			}
 		}
 
-		puts( BG_BRIGHT_RED "Game is over!" RESET_COLOR );
+		ui->vtable->print_winner( winner );
 
-		print_winner( winner );
+		ui->vtable->print_field( game );
 
-		print_field( game );
-
-		printf( "Play again [y/n]? " );
-
-		do
-		{
-			int s = scanf("%c", &answer);
-			if( EOF == s )
-			{
-				return -1;
-			}
-			else if( 0 == s )
-			{
-				while (fgetc(stdin) != '\n');
-			}
-		}
-		while( 'y' != answer && 'n' != answer );
-
+		answer = ui->vtable->read_play_again();
 
 		game_restart( game );
 	} while( answer != 'n');
